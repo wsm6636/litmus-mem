68b7b67a0bdfa167b38384e707f00a31a20c5bc7
wsm6636 <812629965@qq.com>
2019-04-30 21:07:57 +0800

    4.30-4

--- .memga-4.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/wsm/memguard-4.30/memga-4.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/wsm/memguard-4.30/memga-4.ko /home/wsm/memguard-4.30/memga-4.o /home/wsm/memguard-4.30/memga-4.mod.o ;  true
--- make.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo rmmod memga-4.ko
-make clean
-make
-sudo insmod memga-4.ko
-
