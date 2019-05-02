9638d76cfc5e9cbf992a56ed4173c39cde9f7863
wsm6636 <812629965@qq.com>
2019-05-02 09:09:15 +0800

    5.2

--- task/b/set/src/blackscholes.d
+++
@@ -1,7 +0,0 @@
-blackscholes.o blackscholes.d : blackscholes.c \
- /home/wsm/litmus/liblitmus/include/litmus.h \
- /home/wsm/litmus/liblitmus/include/litmus/rt_param.h \
- /home/wsm/litmus/liblitmus/include/litmus/ctrlpage.h \
- /home/wsm/litmus/liblitmus/include/litmus/rt_param.h \
- /home/wsm/litmus/liblitmus/arch/x86/include/asm/cycles.h \
- /home/wsm/litmus/liblitmus/include/migration.h
